8d48ab6013e368cd6e20b81dd5f6289bdf46ff77
Jacob Wilkins <jacob.wilkins@oerc.ox.ac.uk>
2019-07-08 12:15:21 +0100

    Add license info

--- finite_d.py
+++ finite_d.py
@@ -1,3 +1,27 @@
+#-*- coding: utf-8 -*-
+# module finite_d.py
+# MIT License
+
+# Copyright (c) 2019 Jacob Wilkins
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import math
 import numpy
 import itertools
@@ -113,7 +137,7 @@ def finite_d( F, h, order = None, stencil = None, x = None, points = None, prefa
     In:
     F       (function | numpy.ndarray) : Function or grid to differentiate
     stencil (int)                      : Order of stencil
-    h       (float)                    : step length for derivative
+    h       (list(float) | float)      : step length for derivative
     order   (int)                      : Derivative order for automatically generated stencils
     stencil (int)                      : Stencil order for automatically generated stencils
     x       (function arg | slice)     : For grids numpy slice to be acted upon
